da7a33c7fcdb7c05d83db445c3c5d711d4d5806d
julian.cardenas <juliancp20@gmail.com>
2017-12-29 11:27:05 -0600

    Adding Onclick

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,106 +1,58 @@
 "use strict";
 
 var app = {
-    Title: "Indecision App",
-    Subtitle: "put your life in the hands of a computer",
-    options: ["one", "two"]
+  Title: "Indecision App"
 };
-
-var user = {
-    Name: 'Julian Cardenas',
-    Age: 21,
-    Location: 'Monterrey',
-    Places: ["Mexico", "USA", "Chile"],
-    PrintPlacesLived: function PrintPlacesLived() {
-        var _this = this;
-
-        console.log(this.Name);
-        console.log(this.Age);
-        console.log(this.Location);
-        this.Places.forEach(function (city) {
-            console.log(_this.Name + " has lived in " + city);
-        });
-    }
-};
-
-user.PrintPlacesLived();
-
-var getFirstName = function getFirstName(user) {
-    return user.Name.split(" ")[0];
-};
-
-var gfn = function gfn(user) {
-    return user.Name.split(" ")[0];
-};
-
-var gfnlet = function gfnlet(user) {
-    return user.Name.split(" ")[0];
-};
-function getLocation(location) {
-    if (location) return React.createElement(
-        "p",
-        null,
-        "Location: ",
-        location
-    );
-};
-
-console.log("\n");
-console.log(gfnlet(user));
-console.log(gfn(user));
-console.log(getFirstName(user));
 
 var appRoot = document.getElementById("app");
-// templates
-var template = React.createElement(
+
+//properties
+var count = 0;
+
+//functions
+var addOne = function addOne() {
+  count++;
+
+  renderCounterApp();
+};
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+};
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+};
+
+var renderCounterApp = function renderCounterApp() {
+
+  var templateTwo = React.createElement(
     "div",
     null,
     React.createElement(
-        "h1",
-        null,
-        app.Title
-    ),
-    app.Subtitle && React.createElement(
-        "p",
-        null,
-        app.Subtitle
+      "h1",
+      null,
+      "Count: ",
+      count
     ),
     React.createElement(
-        "p",
-        null,
-        app.options.length > 0 ? "Here are your options" : "No options"
+      "button",
+      { onClick: addOne },
+      "+1"
     ),
     React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "li"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "li"
-        )
+      "button",
+      { onClick: minusOne },
+      "-1"
+    ),
+    React.createElement(
+      "button",
+      { onClick: reset },
+      "reset"
     )
-);
+  );
 
-var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        user.Name ? user.Name : "Anonymous"
-    ),
-    user.Age && user.Age >= 18 && React.createElement(
-        "p",
-        null,
-        "Age: ",
-        user.Age + " !"
-    ),
-    getLocation(user.Location)
-);
+  ReactDOM.render(templateTwo, appRoot);
+};
 
-ReactDOM.render(template, appRoot);
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,67 +1,42 @@
 const app = {
-    Title: "Indecision App",
-    Subtitle: "put your life in the hands of a computer",
-    options : ["one","two"]
+    Title: "Indecision App"
 };
-
-const user ={
-    Name: 'Julian Cardenas',
-    Age:21,
-    Location:'Monterrey',
-    Places:["Mexico","USA","Chile"],
-    PrintPlacesLived(){
-     console.log(this.Name);
-     console.log(this.Age);
-     console.log(this.Location)
-     this.Places.forEach((city) => {
-        console.log(this.Name + " has lived in " + city);
-     });
-    }
-};
-
-user.PrintPlacesLived();
-
-const getFirstName =(user) => user.Name.split(" ")[0]; 
-
-const gfn  = (user) => {
-    return user.Name.split(" ")[0];
- }
-
-let gfnlet = (user) => user.Name.split(" ")[0];
-function getLocation(location){
-    if(location)
-    return <p>Location: {location}</p>;
-};
-
-
-
-console.log("\n");
-console.log(gfnlet(user));
-console.log(gfn(user));
-console.log(getFirstName(user));
-
 
 const appRoot = document.getElementById("app");
-// templates
-const template =(  
-    <div>   
-      <h1>{app.Title}</h1>
-     {app.Subtitle && <p>{app.Subtitle}</p>}
-     <p>{app.options.length >0 ? "Here are your options" : "No options"}</p>
-      <ol>
-          <li>li</li>
-          <li>li</li>
-      </ol>
-    </div>
-  );
+
+//properties
+let count=0;
   
+//functions
+  const addOne =() =>{
+      count++;
+    
+      renderCounterApp();
+    };
+  const minusOne =() =>{ 
+      count--;
+      renderCounterApp();
+    };
+    const reset=() =>{
+      count=0;
+      renderCounterApp();
+    };
 
-const templateTwo =(
-    <div>
-    <h1>{ user.Name ? user.Name : "Anonymous" }</h1>
-   { (user.Age && user.Age >= 18) && <p>Age: {user.Age + " !"}</p> }
-   { getLocation(user.Location) }
-    </div>
-);
 
-ReactDOM.render(template,appRoot);
+    const renderCounterApp = () => {
+       
+        const templateTwo =(
+            <div>
+               <h1>Count: {count}</h1>
+               <button onClick={addOne}>+1</button>
+               <button onClick={minusOne}>-1</button>
+               <button onClick={reset}>reset</button>
+            </div>
+           );
+
+           ReactDOM.render(templateTwo,appRoot);
+    };
+
+    renderCounterApp();
+
+
